51b67a31c34a40b199d072094304541f38953fb6
falafel.space <harekrishna+falafel@gmail.com>
2019-08-09 17:24:21 +0000

    updates uri

--- fetch-data.sh
+++ fetch-data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 set -euo pipefail
-wget https://ftp-trace.ncbi.nih.gov/sra/reports/Metadata/NCBI_SRA_Metadata_Full_20181203.tar.gz
+uri=https://ftp-trace.ncbi.nih.gov/sra/reports/Metadata/NCBI_SRA_Metadata_Full_20190629.tar.gz
+wget ${uri}
